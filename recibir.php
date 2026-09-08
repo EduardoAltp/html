ca2dcaf1d4f774381d89ac5d6b0d37e9b4465488
Eduardo <eddiealtamirano01@gmail.com>
2026-09-08 16:16:30 -0600

    Se agrego una pagina de recibir con .php

---
+++ recibir.php
@@ -0,0 +1,17 @@
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Recibir datos</title>
+    </head>
+    <body>
+        <?php
+            $nombre = $_POST['nombre'];
+            $email = $_POST['email'];
+            $fecha_nacimiento = $_POST['fecha_nacimiento'];
+            echo "<h2>Recibido correctamente</h2>";
+            echo "<h1>Hola $nombre, tu email es $email y naciste el $fecha_nacimiento</h1>";   
+        ?>
+    </body>
+</html>
